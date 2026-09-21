a10e7cc070f1989d5b2d5e732cda6b7f93704bfa
A.K.M Nazmul Huda <nhuprim24@gmail.com>
2026-09-21 13:36:14 +0600

    bug fixed

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,45 @@
+// import app from "./app";
+// import { env } from "./app/config/env";
+// import { connectDatabase, disconnectDatabase } from "./app/config/db";
+
+// async function bootstrap() {
+//     try {
+//         await connectDatabase();
+
+//         const server = app.listen(env.port, () => {
+//             // eslint-disable-next-line no-console
+//             console.log(
+//                 `🚀 Server running on http://localhost:${env.port} [${env.nodeEnv}]`,
+//             );
+//         });
+
+//         const shutdown = async (signal: string) => {
+//             // eslint-disable-next-line no-console
+//             console.log(`\n${signal} received. Shutting down gracefully...`);
+//             server.close(async () => {
+//                 await disconnectDatabase();
+//                 process.exit(0);
+//             });
+//         };
+
+//         process.on("SIGINT", () => shutdown("SIGINT"));
+//         process.on("SIGTERM", () => shutdown("SIGTERM"));
+//     } catch (err) {
+//         // eslint-disable-next-line no-console
+//         console.error("❌ Failed to start server:", err);
+//         process.exit(1);
+//     }
+// }
+
+// bootstrap();
 import app from "./app";
-import { env } from "./app/config/env";
-import { connectDatabase, disconnectDatabase } from "./app/config/db";
 
-async function bootstrap() {
-    try {
-        await connectDatabase();
+const port = 8000;
 
-        const server = app.listen(env.port, () => {
-            // eslint-disable-next-line no-console
-            console.log(
-                `🚀 Server running on http://localhost:${env.port} [${env.nodeEnv}]`,
-            );
-        });
-
-        const shutdown = async (signal: string) => {
-            // eslint-disable-next-line no-console
-            console.log(`\n${signal} received. Shutting down gracefully...`);
-            server.close(async () => {
-                await disconnectDatabase();
-                process.exit(0);
-            });
-        };
-
-        process.on("SIGINT", () => shutdown("SIGINT"));
-        process.on("SIGTERM", () => shutdown("SIGTERM"));
-    } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error("❌ Failed to start server:", err);
-        process.exit(1);
-    }
+async function main() {
+    const server = app.listen(port, () => {
+        console.log("App is running at", port);
+    });
 }
 
-bootstrap();
+main();
