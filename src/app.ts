cffa0d7115b39a30a76cf92b9ce75800410dec6a
A.K.M Nazmul Huda <nhuprim24@gmail.com>
2026-09-21 14:26:09 +0600

    bug fixed

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app: Application = express();
 // app.use(cors());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: process.env.CLIENT_URL,
         credentials: true,
     }),
 );
